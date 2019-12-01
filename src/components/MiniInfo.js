1cd0a082b00c05efca737776b192162706bc5340
Evgeny Lositsky <elosyash239@gmail.com>
2019-12-01 17:57:24 +0300

    added componentDidMout

--- src/components/MiniInfo.js
+++ src/components/MiniInfo.js
@@ -2,15 +2,33 @@ import React, {Component} from 'react';
 
 class MiniInfo extends Component {
     state = {
-        cats: 10
+        cats: 10,
+        isLoaded: false,
+        text: ""
     };
 
-
+    componentDidMount() {
+        fetch("http://fizmatspb.ru/itmoweb/lab3/requester.php")
+            .then((response) => {
+                return response.text()
+            })
+            .then((response) => {
+                    this.setState({
+                            isLoaded: true,
+                            text: response
+                        }
+                    )
+                }
+            )
+            .catch(() => {
+                console.log("Fetch Error")
+            })
+    }
 
     increaseCats(sobytie) {
         this.setState({
             cats: this.state.cats + 1
-    });
+        });
     };
 
     render() {
@@ -19,6 +37,8 @@ class MiniInfo extends Component {
                 {this.props.AnimalName}: {this.state.cats}
                 <br/>
                 <button onClick={() => this.increaseCats()}> Increase Cats</button>
+                <br/>
+                {!this.state.isLoaded ? "text is now loading" : this.state.text}
             </div>
         )
     }
